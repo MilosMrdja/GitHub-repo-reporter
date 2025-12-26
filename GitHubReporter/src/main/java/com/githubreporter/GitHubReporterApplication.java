0b4e3bb630063ef586d74ee6a70c7287c953dd9c
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2025-12-26 14:21:53 +0100

    feat(): init

---
+++ GitHubReporter/src/main/java/com/githubreporter/GitHubReporterApplication.java
@@ -0,0 +1,13 @@
+package com.githubreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubReporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubReporterApplication.class, args);
+    }
+
+}
---
+++ GitHubReporter/src/test/java/com/githubreporter/GitHubReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubreporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubReporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
